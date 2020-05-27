c3dafd4ae0557d07f1fe5df2843fb3110194a768
rennokki <rennokki@gmail.com>
2020-05-27 14:08:56 +0300

    Update TestFailingController.php

--- tests/Http/Controllers/TestFailingController.php
+++ tests/Http/Controllers/TestFailingController.php
@@ -9,15 +9,15 @@ class TestFailingController extends HealthcheckController
 {
     public function registerHealthchecks(Request $request)
     {
-        $this->addHealtcheck('test1', function (Request $request) {
+        $this->addHealthcheck('test1', function (Request $request) {
             return true;
         });
 
-        $this->addHealtcheck('test2', function (Request $request) {
+        $this->addHealthcheck('test2', function (Request $request) {
             return false;
         });
 
-        $this->addHealtcheck('test3', function (Request $request) {
+        $this->addHealthcheck('test3', function (Request $request) {
             return false;
         });
     }
